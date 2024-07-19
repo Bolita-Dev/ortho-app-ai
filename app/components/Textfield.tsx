74646b81f3b6f7b6a2b92e443cae822860a3c050
Miguel Viñas <m.vinas@crowdfarming.com>
2024-07-19 20:49:14 +0200

    feat: global style changes

--- app/components/Textfield.tsx
+++ app/components/Textfield.tsx
@@ -1,44 +1,74 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Textfield = () => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [wordCount, setWordCount] = useState<number>(0);
+
+  /*
+   * Dynamically adjust the height of the textarea based on the content
+   * and the maximum number of lines allowed.
+   * If the height is more than the specified lines, the scrollbar appears
+   */
+  const handleInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const textarea = textAreaRef.current;
+
+    if (!textarea) return;
+
+    const maxLines = 8;
+    const lineHeight = parseFloat(getComputedStyle(textarea).lineHeight);
+    const maxHeight = `${maxLines * lineHeight}px`;
+
+    setWordCount(event.target.value.trim().split(/\s+/).filter(Boolean).length);
+
+    textarea.style.height = "auto"; // Reset height in case the user deletes content
+    textarea.style.height = `${textarea.scrollHeight}px`;
+
+    if (textarea.scrollHeight > maxLines * lineHeight) {
+      textarea.style.height = maxHeight;
+      textarea.style.overflowY = "auto";
+    } else {
+      textarea.style.overflowY = "hidden";
+    }
+  };
+
   return (
-    <div className="flex w-full flex-col overflow-hidden border-slate-300 bg-slate-100 text-slate-700 rounded-xl border dark:border-slate-700 dark:bg-slate-800 dark:text-slate-300">
-      <div className="p-2">
-        <p
-          id="promptLabel"
-          className="pb-1 pl-2 text-sm font-bold text-slate-700 opacity-60 dark:text-slate-300">
-          Prompt
-        </p>
-        <p
-          className="scroll-on max-h-44 w-full overflow-y-auto px-2 py-1 focus:outline-none"
-          role="textbox"
-          aria-labelledby="promptLabel"
-          /*      onPaste={handlePaste}
-          ref={promptTextInputRef} */
-          contentEditable></p>
+    <div className="flex w-full flex-col overflow-hidden rounded-xl border border-slate-300 text-slate-700 dark:border-slate-700 dark:text-slate-300 has-[textarea:focus]:outline has-[textarea:focus]:outline-2  has-[textarea:focus]:outline-blue-700">
+      <div className="bg-slate-100/50 dark:bg-slate-800/50 p-2">
         <textarea
-          name="promptText"
-          /* ref={promptTextRef}  */ hidden></textarea>
+          ref={textAreaRef}
+          className=" w-full resize-none bg-transparent p-4 text-m focus:outline-none"
+          name="message"
+          rows={2}
+          placeholder="Escribe o pega aquí la frase a revisar..."
+          onInput={handleInput}
+        ></textarea>
       </div>
-      <div className="flex w-full items-center px-2.5 py-2">
+      <div className="flex w-full items-center justify-between border-t border-slate-300 bg-slate-100 pl-6 pr-2 py-2 dark:border-slate-700 dark:bg-slate-800">
+        <p className="text-sm text-slate-500">
+          {wordCount >= 3
+            ? wordCount + " palabras"
+            : "Escribe por lo menos 3 palabras"}{" "}
+        </p>
         <button
           type="button"
-          className="ml-auto flex cursor-pointer items-center gap-2 whitespace-nowrap bg-blue-700 px-4 py-2 text-center text-xs font-medium tracking-wide text-slate-100 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:opacity-100 active:outline-offset-0 disabled:cursor-not-allowed disabled:opacity-75 dark:bg-blue-600 dark:text-slate-100 dark:focus-visible:outline-blue-600 rounded-xl"
+          className="ml-auto flex bg-gradient-to-r from-purple-600 text-sm font-medium to-pink-600 cursor-pointer items-center gap-2 whitespace-nowrap  px-2 py-2 text-center  tracking-wide text-slate-100 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:opacity-100 active:outline-offset-0 disabled:cursor-not-allowed disabled:opacity-75  dark:text-slate-100 dark:focus-visible:outline-blue-600 rounded-xl"
           /*  onClick={handleClick} */
+          disabled={wordCount < 3}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="size-3"
-            aria-hidden="true">
+            className="size-4"
+            aria-hidden="true"
+          >
             <path
               fillRule="evenodd"
               d="M5 4a.75.75 0 0 1 .738.616l.252 1.388A1.25 1.25 0 0 0 6.996 7.01l1.388.252a.75.75 0 0 1 0 1.476l-1.388.252A1.25 1.25 0 0 0 5.99 9.996l-.252 1.388a.75.75 0 0 1-1.476 0L4.01 9.996A1.25 1.25 0 0 0 3.004 8.99l-1.388-.252a.75.75 0 0 1 0-1.476l1.388-.252A1.25 1.25 0 0 0 4.01 6.004l.252-1.388A.75.75 0 0 1 5 4ZM12 1a.75.75 0 0 1 .721.544l.195.682c.118.415.443.74.858.858l.682.195a.75.75 0 0 1 0 1.442l-.682.195a1.25 1.25 0 0 0-.858.858l-.195.682a.75.75 0 0 1-1.442 0l-.195-.682a1.25 1.25 0 0 0-.858-.858l-.682-.195a.75.75 0 0 1 0-1.442l.682-.195a1.25 1.25 0 0 0 .858-.858l.195-.682A.75.75 0 0 1 12 1ZM10 11a.75.75 0 0 1 .728.568.968.968 0 0 0 .704.704.75.75 0 0 1 0 1.456.968.968 0 0 0-.704.704.75.75 0 0 1-1.456 0 .968.968 0 0 0-.704-.704.75.75 0 0 1 0-1.456.968.968 0 0 0 .704-.704A.75.75 0 0 1 10 11Z"
               clipRule="evenodd"
             />
           </svg>
-          Generate
+          Revisar ortografía
         </button>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         }),
       }),
       prompt: `Revisa si la siguiente frase tiene faltas de ortografía 
-      y devuelve un fichero json con la frase corregida y un array con de 
+      y devuelve un fichero json con la frase corregida y un array con 
       las palabras que estaban mal y muestra su versión errónea y correcta y 
       la explicación de la real academia española de la lengua expicando la regla 
       ortográfica que está incumpliendo: ${input}`,
@@ -45,29 +45,30 @@ export default function Home() {
     event.preventDefault();
     setInput(event.target.value);
   };
-  return (
-    <main className="bg-slate-900 h-screen p-2">
-      <div className="flex flex-col justify-center items-center">
-        <Card>
-          <Textfield />
-        </Card>
 
-        <div className="flex w-full max-w-2xl gap-4 overflow-x-auto pb-4">
+  return (
+    <main className="bg-slate-900 h-screen p-4">
+      <div className="flex flex-col gap-4 items-center">
+        <div className="flex w-full max-w-3xl">
+          <Textfield />
+        </div>
+
+        <div className="flex w-full max-w-3xl gap-4">
           <Card>
-            <div className="flex items-center gap-2 text-black dark:text-white">
-              <span className="text-sm font-bold">Original text</span>
+            <div className="dark:text-slate-500 mb-2">
+              <span className="text-sm font-semibold">Texto revisado</span>
             </div>
-            <p className="text-pretty mt-4 sm:mt-0 text-sm text-slate-700 dark:text-slate-300">
+            <p className="text-pretty text-m text-slate-700 dark:text-slate-300">
               A magician who turns caffeine into code, pizza into programs, and
               stress into syntax errors.
             </p>
           </Card>
 
           <Card>
-            <div className="flex items-center gap-2 text-black dark:text-white">
-              <span className="text-sm font-bold">Reviewed text</span>
+            <div className="dark:text-slate-500 mb-2">
+              <span className="text-sm font-semibold">Texto original</span>
             </div>
-            <p className="text-pretty mt-4 sm:mt-0 text-sm text-slate-700 dark:text-slate-300">
+            <p className="text-pretty text-m text-slate-700 dark:text-slate-300">
               Someone who talks to computers in their own language and
               occasionally gets a response.
             </p>
