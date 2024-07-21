0908a6034b26b66a608538772d3fdafe84036966
Miguel Viñas <m.vinas@crowdfarming.com>
2024-07-21 12:04:26 +0200

    feat: add tailwind prettier plugin

--- app/components/ResultsCard.tsx
+++ app/components/ResultsCard.tsx
@@ -6,18 +6,18 @@ const ResultsCard = ({
   children?: ReactElement | ReactElement[];
 }) => {
   return (
-    <div className="results-box w-full flex gap-4 rounded-xl  p-4 text-sm text-slate-100 dark:bg-blue-600 dark:text-slate-100">
+    <div className="results-box hols flex w-full gap-4 rounded-xl p-4 text-sm text-slate-100 dark:bg-blue-600 dark:text-slate-100">
       <div className="flex flex-1 flex-wrap">
         <div className="percentage flex flex-1 flex-col justify-center">
-          <p className="font-bold text-4xl flex justify-center">35%</p>
+          <p className="flex justify-center text-4xl font-bold">35%</p>
           <p className="flex justify-center">correcto</p>
         </div>
         <div className="errors flex flex-1 flex-col justify-center">
-          <p className="font-bold text-4xl flex justify-center">5</p>
+          <p className="flex justify-center text-4xl font-bold">5</p>
           <p className="flex justify-center">errores</p>
         </div>
       </div>
-      <p className="flex-1 flex gap-2 italic text-base">
+      <p className="flex flex-1 gap-2 text-base italic">
         Houston, tenemos un problema ortográfico... ¡y no tenemos suficiente
         cinta adhesiva para arreglarlo!
       </p>
