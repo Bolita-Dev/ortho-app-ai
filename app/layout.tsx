bef4341f3bb3646a452fbea1b949489dc7156111
Miguel Viñas <m.vinas@crowdfarming.com>
2024-07-24 21:14:41 +0200

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Ortho',
   description: 'Revisa tus faltas de ortografía',
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
