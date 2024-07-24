b0be21b3d2678014d28ed4d9ae327c9ed0d4617e
elisagc <elisagcubero@gmail.com>
2024-07-24 21:23:57 +0200

    feat: text with colors

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,8 @@ export default function Home() {
   const setGeneratedResponse = (response: GeneratedResponse) => {
     setGenerated(response);
   };
+  const correctWord = (word: string) =>
+    word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 
   return (
     <ToastProvider>
@@ -48,7 +50,20 @@ export default function Home() {
               </div>
 
               <p className="text-m text-pretty text-slate-700 dark:text-slate-300">
-                {originalText}
+                {originalText.split(' ').map((word, index) => {
+                  const cleanWord = correctWord(word);
+                  const isCorrected = corrections.some(
+                    correction =>
+                      correctWord(correction.originalWord) === cleanWord
+                  );
+                  return (
+                    <span
+                      className={`text-m text-pretty ${isCorrected ? 'cursor-pointer text-red-600' : 'text-slate-700 dark:text-slate-300'}`}
+                      key={index}>
+                      {word}{' '}
+                    </span>
+                  );
+                })}
               </p>
             </Card>
             <Card>
@@ -56,7 +71,20 @@ export default function Home() {
                 <span className="text-sm">Texto corregido</span>
               </div>
               <p className="text-m text-pretty text-slate-700 dark:text-slate-300">
-                {correctedText}
+                {correctedText.split(' ').map((word, index) => {
+                  const cleanWord = correctWord(word);
+                  const isCorrected = corrections.some(
+                    correction =>
+                      correctWord(correction.correctedWord) === cleanWord
+                  );
+                  return (
+                    <span
+                      className={`text-m text-pretty ${isCorrected ? 'cursor-pointer text-green-600' : 'text-slate-700 dark:text-slate-300'}`}
+                      key={index}>
+                      {word}{' '}
+                    </span>
+                  );
+                })}
               </p>
               <div className="absolute right-2 top-2">
                 <CopyToClipBoard textToCopy={correctedText} />
